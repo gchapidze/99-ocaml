4c729468b6296be0ebb6c95aca52ca27fe84d4a1
Giorgi Chapidze <chapidze_g@yahoo.com>
2023-05-05 17:08:08 +0100

    twenty 5th problem

--- head/lib/ex_25.ml
+++ head/lib/ex_25.ml
@@ -1,3 +1,20 @@
 (* # permutation ["a"; "b"; "c"; "d"; "e"; "f"];;
 - : string list = ["c"; "d"; "f"; "e"; "b"; "a"] *)
 
+(* |ln - x|ln - x + 1|ln - x + 2| ... | ln - (x + n) >= 0 *)
+let remove_at ls i = 
+  let rec remover l acc j = 
+    match l with
+    | [] -> acc
+    | x :: xs -> if i = j then remover(xs)(acc)(j + 1) else remover(xs)(x :: acc)(j + 1)
+  in
+  List.rev @@ remover ls [] 0;;
+
+  let permutation ls = 
+  let rec generate l acc = 
+    if List.length l = 0 then acc 
+    else 
+      let index = Random.int(List.length l) in 
+      generate(remove_at l index)(List.nth l index :: acc)
+  in
+  generate ls [];; 
